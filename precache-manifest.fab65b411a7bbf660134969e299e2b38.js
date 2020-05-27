292daf919122a3bf48cbad28637aaf0a02b881a2
YuYing_Liang <yuying.liang00@gmail.com>
2020-05-27 19:15:24 -0400

    Updates

---
+++ precache-manifest.fab65b411a7bbf660134969e299e2b38.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7126b35ba1c68651920142e7bfa4c42",
+    "url": "/frosh_2t0_frontend/index.html"
+  },
+  {
+    "revision": "2f0592825e47755d01dd",
+    "url": "/frosh_2t0_frontend/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "aa11624cfcdfb67a5f4e",
+    "url": "/frosh_2t0_frontend/static/css/main.1939f846.chunk.css"
+  },
+  {
+    "revision": "2f0592825e47755d01dd",
+    "url": "/frosh_2t0_frontend/static/js/2.d5580be2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/frosh_2t0_frontend/static/js/2.d5580be2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa11624cfcdfb67a5f4e",
+    "url": "/frosh_2t0_frontend/static/js/main.a3dd8b8f.chunk.js"
+  },
+  {
+    "revision": "a40ecfd46ed230a471df",
+    "url": "/frosh_2t0_frontend/static/js/runtime-main.aa172ed4.js"
+  },
+  {
+    "revision": "dc9c50282ba13d5b623b9c82ce07bde6",
+    "url": "/frosh_2t0_frontend/static/media/2T0Logo.dc9c5028.svg"
+  },
+  {
+    "revision": "84d633877b719f8d50722b2d822d2dd7",
+    "url": "/frosh_2t0_frontend/static/media/blog.84d63387.svg"
+  },
+  {
+    "revision": "f7f8984f25ce581f4cf73fe709f8d79f",
+    "url": "/frosh_2t0_frontend/static/media/bugbear.f7f8984f.png"
+  },
+  {
+    "revision": "34e73505be53a83b3d32236dab04f124",
+    "url": "/frosh_2t0_frontend/static/media/facebook.34e73505.svg"
+  },
+  {
+    "revision": "6c6931f426bcc5ba571c8ed56b757ece",
+    "url": "/frosh_2t0_frontend/static/media/instagram.6c6931f4.svg"
+  },
+  {
+    "revision": "0fdb4aabcdbd9bb61aa52753eb0c9836",
+    "url": "/frosh_2t0_frontend/static/media/snapchat.0fdb4aab.svg"
+  },
+  {
+    "revision": "a1223de504180a12574a17a1f30c5a08",
+    "url": "/frosh_2t0_frontend/static/media/twitter.a1223de5.svg"
+  },
+  {
+    "revision": "ed4ab1c143e08475d765765ab9d6de74",
+    "url": "/frosh_2t0_frontend/static/media/youtube.ed4ab1c1.svg"
+  }
+]);
