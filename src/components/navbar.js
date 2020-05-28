48da5719e9bdc4fe09ae3dc97273eb581fe6b49d
jameskokoska <jameskokoska@users.noreply.github.com>
2020-05-27 22:43:08 -0400

    added landing page overlay

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react'
 import '../css/components/navbar.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import {Jumbotron, Button, Navbar, Nav, NavDropdown,Form,FormControl} from 'react-bootstrap';
-import navbarIcon from "./assets/2T0Logo.svg"
+import navbarIcon from "./assets/main_logo_transparent.png"
 import blogIcon from "./assets/blog.svg"
 import facebookIcon from "./assets/facebook.svg"
 import instagramIcon from "./assets/instagram.svg"
@@ -14,20 +14,20 @@ import {Link} from 'react-router-dom';
 class NavbarComponent extends Component {
   render() {
     return (
-    <Navbar bg="light" expand="md" fixed="top" >
-      <Navbar.Brand href="#home" style={{position: 'absolute'}} >
-        <img src={navbarIcon} className="d-inline-block align-top" alt="Frosh Week Icon" width="30" height="30"/>
-      </Navbar.Brand>
-      <div class="sticky-top" style={{position: 'absolute', display: 'inline-block', float: 'right', right:'0vw'}}>
+    <Navbar expand="md" fixed="top" >
+      <div className="navbarIcon" style={{position: 'absolute'}} >
+        <img src={navbarIcon} className="d-inline-block align-top" alt="Frosh Week Icon" width="100%" height="100%"/>
+      </div>
+      <div className="sticky-top" style={{position: 'absolute', display: 'inline-block', float: 'right', right:'0vw'}}>
         <Navbar.Toggle aria-controls="navbar-id" class="sticky-top"/>
       </div>
       <Navbar.Collapse id="navbar-id" className="justify-content-center">
-        <div class="navbarLink navbarSelected"><Nav.Link href="/">Home</Nav.Link></div>
-        <div class="navbarLink"><Nav.Link href="/register">Registration</Nav.Link></div>
-        <div class="navbarLink"><Nav.Link href="/about">About Us</Nav.Link></div>
-        <div class="navbarLink"><Nav.Link href="/contact">Contact Us</Nav.Link></div>
+        <div className="navbarLink navbarSelected"><Nav.Link href="/">Home</Nav.Link></div>
+        <div className="navbarLink"><Nav.Link href="/register">Registration</Nav.Link></div>
+        <div className="navbarLink"><Nav.Link href="/about">About Us</Nav.Link></div>
+        <div className="navbarLink"><Nav.Link href="/contact">Contact Us</Nav.Link></div>
       </Navbar.Collapse>
-        <div class="navbarSocialsContainer">
+        <div className="navbarSocialsContainer">
           <img src={instagramIcon} alt="Instagram"></img>
           <img src={snapchatIcon} alt="Snapchat"></img>
           <img src={facebookIcon} alt="Facebook"></img>
--- src/components/slideshow.js
+++ src/components/slideshow.js
@@ -1,27 +1,26 @@
 import React, {Component} from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '../css/components/slideshow.css'
+import spaceImg from "./assets/space.png"
 import {Jumbotron, Button, Carousel} from 'react-bootstrap';
 
 class Slideshow extends Component {
   render() {
     return (
+        
         <Carousel interval={5000}>
             <Carousel.Item>
-                <div class="slideshowLandingBG">
-                    <br/><br/><br/>
-                    <div class="slideshowButton">
-                        Register Now!
-                    </div>
+                <div className="slideshowSlide">
+                    <img src={spaceImg}></img>
                 </div>
             </Carousel.Item>
             <Carousel.Item>
-                <div class="slideshowSlide">
+                <div className="slideshowSlide">
                     <img src="https://photos.smugmug.com/1T9-2T0/Frosh-Week/Frosh-Events/Cheer-off/i-gGx2t7W/0/41239513/XL/_IGP8970-XL.jpg"></img>
                 </div>
             </Carousel.Item>
             <Carousel.Item>
-                <div class="slideshowBG"></div>
+                <div className="slideshowBG"></div>
             </Carousel.Item>
         </Carousel>
     )
